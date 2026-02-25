3e0170033cf5a6dab77dd65dd4d672f1b7c967b9
yunamail7 <yunamail7@gmail.com>
2026-02-26 06:14:09 +0800

    chore: add build timestamp to force cache bypass

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,5 @@
 import { StrictMode } from 'react'
+console.log('BUILD_B', new Date().toISOString())
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
